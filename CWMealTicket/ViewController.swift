fd9271862c66bcb73e1ab989b8d3496c347ad2c7
ataka <masayuki.ataka@gmail.com>
2015-02-12 00:51:13 +0900

    Init swift project

---
+++ CWMealTicket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CWMealTicket
+//
+//  Created by 安宅 正之 on 2015/02/11.
+//  Copyright (c) 2015年 MasayukiAtaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
